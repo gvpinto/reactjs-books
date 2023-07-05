2a0425aca3277d10c01cf38d4750514fad9eae89
Piercing Stripes <gvpinto@gmail.com>
2023-07-04 22:37:04 -0400

    6.88 - Receiving new Titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import BookCreate from './components/BookCreate';
 
 function App() {
 
     const [books, setBooks] = useState([]);
 
+    const createBook = (title) => {
+        console.log('Book created', title);
+    };
+
     return (
-        <div>App</div>
+        <BookCreate onCreate={createBook}></BookCreate>
     );
 }
 
