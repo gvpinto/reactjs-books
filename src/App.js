78e45daea45f4d61f57dcf691f9123b7c80f23d9
Glenn Pinto <gvpinto@gmail.com>
2023-07-06 23:01:30 -0400

    Completed section 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -6,17 +8,34 @@ function App() {
 
     const [books, setBooks] = useState([]);
 
-    const deleteBookById = (id) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book, index) => {
             return book.id !== id;
         });
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle };
+                // return { ...book, title: newTitle };
+                return { ...book, ...response.data };
             }
 
             return book;
@@ -25,10 +44,18 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+        // const updatedBooks = [
+        //     ...books,
+        //     { id: Math.round(Math.random() * 9999), title }
+        // ];
         const updatedBooks = [
             ...books,
-            { id: Math.round(Math.random() * 9999), title }
+            response.data
         ];
         setBooks(updatedBooks);
     };
